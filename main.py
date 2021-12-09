4772d5ce9c5af8fc80f368c482a1a2de17965c28
Siddhartha Malladi <msiddhu45@gmail.com>
2021-12-09 11:46:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,115 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import io
+import os
+import docx2txt
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFSyntaxError
+
+
+
+def extract_text_from_docx(doc_path):
+    try:
+        temp = docx2txt.process(doc_path)
+        text = [line.replace('\t', ' ') for line in temp.split('\n') if line]
+        return ' '.join(text)
+    except KeyError:
+        return ' '
+
+
+def extract_text_from_doc(doc_path):
+    try:
+        try:
+            import textract
+        except ImportError:
+            return ' '
+        text = textract.process(doc_path).decode('utf-8')
+        return text
+    except KeyError:
+        return ' '
+
+
+def extract_text_from_pdf(pdf_path):
+    if not isinstance(pdf_path, io.BytesIO):
+        with open(pdf_path, 'rb') as fh:
+            try:
+                for page in PDFPage.get_pages(
+                        fh,
+                        caching=True,
+                        check_extractable=True
+                ):
+                    resource_manager = PDFResourceManager()
+                    fake_file_handle = io.StringIO()
+                    converter = TextConverter(
+                        resource_manager,
+                        fake_file_handle,
+                        laparams=LAParams()
+                    )
+                    page_interpreter = PDFPageInterpreter(
+                        resource_manager,
+                        converter
+                    )
+                    page_interpreter.process_page(page)
+
+                    text = fake_file_handle.getvalue()
+                    yield text
+
+                    converter.close()
+                    fake_file_handle.close()
+            except PDFSyntaxError:
+                return
+    else:
+        try:
+            for page in PDFPage.get_pages(
+                    pdf_path,
+                    caching=True,
+                    check_extractable=True
+            ):
+                resource_manager = PDFResourceManager()
+                fake_file_handle = io.StringIO()
+                converter = TextConverter(
+                    resource_manager,
+                    fake_file_handle,
+                    codec='utf-8',
+                    laparams=LAParams()
+                )
+                page_interpreter = PDFPageInterpreter(
+                    resource_manager,
+                    converter
+                )
+                page_interpreter.process_page(page)
+
+                text = fake_file_handle.getvalue()
+                yield text
+
+                converter.close()
+                fake_file_handle.close()
+        except PDFSyntaxError:
+            return
+
+
+def extract_text(file_path):
+    extension=os.path.splitext(file_path)[1].split('.')[1]
+    text = ''
+    if extension == 'pdf':
+        for page in extract_text_from_pdf(file_path):
+            text += ' ' + page
+    elif extension == 'docx':
+        text = extract_text_from_docx(file_path)
+    elif extension == 'doc':
+        text = extract_text_from_doc(file_path)
+    return text
+
+
+
+print(extract_text("/Users/msiddhu/Desktop/projects/resumeparser/Siddhartha-M-Resume.pdf"))
+
+
